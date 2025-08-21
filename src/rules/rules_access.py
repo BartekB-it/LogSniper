fb172addd79f2f151ccef4fa8c999b5096d89b08
Bartłomiej Biskupiak <biskupiakb@gmail.com>
2025-08-21 21:06:10 +0200

    WIP: New detection rules + severity system

--- src/rules/rules_access.py
+++ src/rules/rules_access.py
@@ -6,32 +6,90 @@ list_of_ips = {}
 
 def classify_access_log(access_log_parsed):
 
-   
+    def higher_severity(access_log_parsed):
+
+        if access_log_parsed["severity"] == "high":
+            return access_log_parsed
+
+        if access_log_parsed["severity"] == "mid":
+            access_log_parsed["severity"] = "high"
+            return access_log_parsed
+
+        if access_log_parsed["severity"] == "low":
+            access_log_parsed["severity"] = "mid"
+            return access_log_parsed
+
+        if access_log_parsed["severity"] == "info":
+            access_log_parsed["severity"] = "low"
+            return access_log_parsed
+        
+    def add_severity_reason(access_log_parsed, severity_reason):
+        if severity_reason == "N/A":
+            access_log_parsed["severity_reason"] == severity_reason
+            return access_log_parsed
+        elif "attempts".find(access_log_parsed["severity_reason"]) > 0 :
+            access_log_parsed["severity_reason"] == severity_reason
+            return access_log_parsed
+        else:
+            access_log_parsed["severity_reason"] += " & "
+            access_log_parsed["severity_reason"] += severity_reason
+            return access_log_parsed
 
     if access_log_parsed:
         for key in access_log_parsed:
             if key == "status": #HTTP error burst - fuzzing / directory bruteforce
                 if access_log_parsed["status"] == "404" or access_log_parsed["status"] == "401" or access_log_parsed["status"] == "403":
-
-                    add_ip = access_log_parsed["ip"]
-
                     if access_log_parsed["ip"] in list_of_ips:
-                        list_of_ips[add_ip] += 1
-                        if list_of_ips[add_ip] > 2:
-                            access_log_parsed["404/401/403_attempts"] = list_of_ips[add_ip]
+                        list_of_ips[access_log_parsed["ip"]] += 1
+                        if list_of_ips[access_log_parsed["ip"]] > 2:
+                            higher_severity(access_log_parsed)
+                            add_severity_reason(access_log_parsed, "more than 2 failed login attempts")
+                            access_log_parsed["404/401/403_attempts"] = list_of_ips[access_log_parsed["ip"]]
+                            suspicious_events.append(access_log_parsed)
+
+                        if list_of_ips[access_log_parsed["ip"]] > 4:
+                            suspicious_events.remove(access_log_parsed)
+                            higher_severity(access_log_parsed)
+                            add_severity_reason(access_log_parsed, "more than 4 failed login attempts")
+                            access_log_parsed["404/401/403_attempts"] = list_of_ips[access_log_parsed["ip"]]
+                            suspicious_events.append(access_log_parsed)
+
+                        if list_of_ips[access_log_parsed["ip"]] > 9:
+                            suspicious_events.remove(access_log_parsed)
+                            higher_severity(access_log_parsed)
+                            add_severity_reason(access_log_parsed, "more than 9 failed login attempts")
+                            access_log_parsed["404/401/403_attempts"] = list_of_ips[access_log_parsed["ip"]]
                             suspicious_events.append(access_log_parsed)
                     else:
-                        list_of_ips[add_ip] = 1
-                    if list_of_ips[add_ip] > 99: #High request rate per IP -> could be DDoS / fuzzing
+                        list_of_ips[access_log_parsed["ip"]] = 1
+
+                    if list_of_ips[access_log_parsed["ip"]] > 99: #High request rate per IP -> could be DDoS / fuzzing
+                        higher_severity(access_log_parsed)
+                        add_severity_reason(access_log_parsed, "high request rate per IP (potential DDoS / fuzzing)")
                         suspicious_events.append(access_log_parsed)
+
             if key == "path": #suspicious URls -> higher severity
                 if ("phpmyadmin" or "/etc/passwd" or "/wp-login.php" or "/admin") in access_log_parsed["path"]:
+                    higher_severity(access_log_parsed)
+                    add_severity_reason(access_log_parsed, "suspicious URl")
                     suspicious_events.append(access_log_parsed)
+
             if key == "path": #SQLi / command injection patterns
                 if ("' OR 1=1 --" or ";wget" or "union select") in access_log_parsed["path"]:
+                    higher_severity(access_log_parsed)
+                    add_severity_reason(access_log_parsed, "SQLi / command injection pattern")
                     suspicious_events.append(access_log_parsed)
+
             if key == "user_agent": #anomalous User-Agent
                 if ("curl" or "python-requests" or "sqlmap" or "nikto") in access_log_parsed["user_agent"]:
+                    higher_severity(access_log_parsed)
+                    add_severity_reason(access_log_parsed, "anomalous user-agent (potential scanner)")
+                    suspicious_events.append(access_log_parsed)
+
+            if key == "method": #strange method
+                if ("DELETE" or "PATCH" or "HEAD" or "OPTIONS" or "TRACE" or "CONNECT") in access_log_parsed["method"]:
+                    higher_severity(access_log_parsed)
+                    add_severity_reason(access_log_parsed, "strange method")
                     suspicious_events.append(access_log_parsed)
         return suspicious_events
     else:
